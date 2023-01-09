b53f86a3bfc1342683a802bd09acf221e9e73afa
Chris Godwin <patchshorts@gmail.com>
2023-01-08 22:53:02 -0600

    simplified easter

--- src/celtibot.py
+++ src/celtibot.py
@@ -201,32 +201,8 @@ def getInfoObjectsForToday(yamlListOfTopics):
     return topics
 
 def calcEasterDate(year):
-    """returns the date of Easter Sunday of the given yyyy year"""
-    y = int(year)
-    # golden year - 1
-    g = y % 19
-    # offset
-    e = 0
-    # century
-    c = y/100
-    # h is (23 - Epact) mod 30
-    h = (c-c/4-(8*c+13)/25+19*g+15)%30
-    # number of days from March 21 to Paschal Full Moon
-    i = h-(h/28)*(1-(h/28)*(29/(h+1))*((21-g)/11))
-    # weekday for Paschal Full Moon (0=Sunday)
-    j = (y+y/4+i+2-c+c/4)%7
-    # number of days from March 21 to Sunday on or before Paschal Full Moon
-    # p can be from -6 to 28
-    p = i-j+e
-    d = int(1+(p+27+(p+6)/40)%31)
-    m = int(3+(p+26)/30)
-    return datetime.date(y,m,d)
-  
-    if year in special_years:
-        dateofeaster = (22 + d + e) - specyr_sub
-    else:
-        dateofeaster = 22 + d + e
-    return dateofeaster
+    from dateutil.easter import *
+    return easter(int(year))
 
 def matchDateFormat(dateString):
     import re
